8727b1424eaa86ebf0a73d10146b51a95d084447
Ritik Singh <ritiksingh.4821@gmail.com>
2022-07-08 11:07:54 +0530

    added implementation for diff.java

--- diff.java
+++ diff.java
@@ -1,6 +1,9 @@
 public class diff{
     
     public static void main(String[] args) {   
-       
+        int a = 5;
+        int b = 4;
+        
+        System.out.println("The Diff is"+ (a - b));
     }
 }
--- multiply.jav
+++
@@ -1,8 +0,0 @@
-public class mulitply() {
-    public static void main(String[] args) {   
-        int a = 5;
-        int b = 4;
-        
-        System.out.println("The Product is "+ (a*b));
-    }
-}
